b5d31b4f5c070418bf6f141336f308232a9d5bdd
axthi2004 <160604520+axthi2004@users.noreply.github.com>
2025-02-28 15:04:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 class Main {
     public static void main(String[] args) {
         int[][] arr = { {3, 8, 5}, {12, 7, 9}, {6, 14, 10} };
