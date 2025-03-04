c4c367b541eacf138c464ca1f2054dbb8a75208b
aathi <160604520+axthi2004@users.noreply.github.com>
2025-03-04 15:30:54 +0530

    harshad.java

--- harshad.java
+++ harshad.java
@@ -1,17 +1,12 @@
-
-public class harshad {
-  public static void main(String[] args){
-  
+class harshad {
+  public static void main(String[] args){ 
 int n=500;
-//for(int i=n;i<=2500;i++){
   int sum=0;
   while (n!=0) {
-    
     int rem=n%10;
     sum+=rem;
     n/=10;
   }
   System.out.println(sum);
 }  
-//}
 }
