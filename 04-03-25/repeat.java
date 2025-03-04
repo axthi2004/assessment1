f92df790c9837922410e4ebc8212b8a63895d45f
aathi <160604520+aathi1412@users.noreply.github.com>
2025-03-04 16:13:04 +0530

    repeat.java

--- 04-03-25/repeat.java
+++ 04-03-25/repeat.java
@@ -1,1 +1,22 @@
-
+class repeat {
+    public static void main(String[] args) {
+        String input = "t6hj7ui";
+        StringBuilder output = new StringBuilder();
+        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            
+            if (Character.isDigit(ch)) {
+                int repeatCount = Character.getNumericValue(ch); 
+                char previousChar = input.charAt(i - 1); 
+                for (int j = 0; j < repeatCount; j++) {
+                    output.append(previousChar); 
+                }
+            } else {
+                output.append(ch); 
+            }
+        }
+        
+        System.out.println(output.toString());
+    }
+}
